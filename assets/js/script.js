a3b26eea73d4cae9e659ed4fcd2b3aa996bb7675
AndreaDeCeglia <deceglia89@gmail.com>
2022-11-04 14:48:50 +0100

    array created

---
+++ assets/js/script.js
@@ -0,0 +1,41 @@
+// objects' container array
+//each obj contains team member's infos
+const team = [
+    // first obj
+    {
+      name: 'Wayne Barnett',
+      role: 'Founder & CEO',
+      image: 'wayne-barnett-founder-ceo.jpg',
+    },
+    // second obj
+    {
+      name: 'Angela Caroll',
+      role: 'Chief Editor',
+      image: 'angela-caroll-chief-editor.jpg',
+    },
+    // third obj
+    {
+      name: 'Walter Gordon',
+      role: 'Office Manager',
+      image: 'walter-gordon-office-manager.jpg',
+    },
+    // fourth obj
+    {
+      name: 'Angela Lopez',
+      role: 'Social Media Manager',
+      image: 'angela-lopez-social-media-manager.jpg',
+    },
+    // fivth obj
+    {
+      name: 'Scott Estrada',
+      role: 'Developer',
+      image: 'scott-estrada-developer.jpg',
+    },
+    // sixth obj
+    {
+      name: 'Barbara Ramos',
+      role: 'Graphic Designer',
+      image: 'barbara-ramos-graphic-designer.jpg',
+    },
+  ];
+  // array's ending
