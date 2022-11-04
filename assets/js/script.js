2b474911dc7c4c477abf625a4193401e3bc45f94
AndreaDeCeglia <deceglia89@gmail.com>
2022-11-04 15:16:31 +0100

    obj's values debug done

--- assets/js/script.js
+++ assets/js/script.js
@@ -36,6 +36,23 @@ const team = [
       name: 'Barbara Ramos',
       role: 'Graphic Designer',
       image: 'barbara-ramos-graphic-designer.jpg',
-    },
-  ];
+    }
+  ]
+
+  console.log(`the array is :`, team);
+  
+  //console.log(`the array's values are :`, team[i]);
+  //the last console.log is wrong 'couse i didn't defind 'i'
   // array's ending
+
+  //imgs' path variable
+  let path = `./../img/`;
+  console.log(`the path in order to reach the images is :`, path);
+
+  //loop for check the array's elements
+  //in this case, objects
+  for( let i=0; i<team.length; i++ ){
+    // getting specific obj each loop
+    let thisObject = team[i];
+    console.log(`name is :`, thisObject.name, ` role is :`, thisObject.role, `img name is :`, thisObject.image);
+  }
